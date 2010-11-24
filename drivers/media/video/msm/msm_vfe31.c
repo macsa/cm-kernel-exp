a2b2880621bda9efab5ad6d124c6f85dd3318aa5
Steve Kondik <shade@chemlab.org>
2010-11-24 00:17:31 -0500

    msm: vfe31: Remove unnecessary request_axi_qos call
    Change-Id: Ib0420a4e2eaf396c588d95d81693d8a8205db905

--- drivers/media/video/msm/msm_vfe31.c
+++ drivers/media/video/msm/msm_vfe31.c
@@ -2683,11 +2683,6 @@ static int vfe31_init(struct msm_vfe_callback *presp,
 		return rc;
 	}
 
-		/* Set required axi bus frequency */
-	rc = request_axi_qos(MSM_AXI_QOS_PREVIEW);
-	if (rc < 0)
-		CDBG("request of axi qos failed\n");
-
 	/* Bring up all the required GPIOs and Clocks */
 	rc = msm_camio_enable(dev);
 	if (rc < 0)
